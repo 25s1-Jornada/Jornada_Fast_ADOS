00762bdc58fdb31550e9fce340c6c5531cb7838b
RenatoSussumu <renatosussumutakahashijunior@gmail.com>
2025-05-07 21:22:35 -0300

    docker

--- Backend/src/api/Analise.py
+++ Backend/src/api/Analise.py
@@ -4,3 +4,17 @@ from src.models.database import AsyncSessionLocal
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from src.models.OS import OS
+
+
+router = APIRouter(prefix="/OS")
+
+async def get_db():
+    db = AsyncSessionLocal()
+    try:
+        yield db
+    finally:
+       await db.close()
+
+@router.get("/bets")
+async def list_bets(AsyncSession = Depends(get_db)):
+    return [1]
