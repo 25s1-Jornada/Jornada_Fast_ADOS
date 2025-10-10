e144658ad68fb5a127029f66099456ac070b9ed9
Felipe <felypebueno2004@gmail.com>
2025-10-09 23:52:45 -0300

    Fix: axios import

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -2,12 +2,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000", 
-  timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: "http://localhost:8000", // ou o IP da sua máquina com FastAPI
 });
-
-
-
