86422b756588c8983d11d7b03d198e64b76c4a84
Felipe_Bueno <110058530+Felipe-Bueno04@users.noreply.github.com>
2025-11-11 21:57:42 -0300

    Add files via upload

--- Backend/app.py
+++ Backend/app.py
@@ -36,5 +36,5 @@ def get_data():
         }
         return jsonify(data)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=False)
